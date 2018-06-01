39afd74bdd05893f7162fdb7355751dfefec2601
ArneilPaulPolican <policanarneilpaul@gmail.com>
2018-06-01 09:48:53 +0800

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]=[
+    { id:11, name: 'Mr nice' },
+    { id:12, name: 'Sample1'},
+    { id:13, name: 'Sample2'},
+    { id:14, name: 'SAmple3'},
+    { id:15, name: 'Sample5' },
+    { id:16, name: 'Sample6'},
+    { id:17, name: 'Sample7'},
+    { id:18, name: 'SAmple8'},
+    { id:19, name: 'Sample9' },
+    { id:20, name: 'Sample20'}
+];
